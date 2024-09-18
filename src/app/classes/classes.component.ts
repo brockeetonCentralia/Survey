5dac9a5c1641b1276d672eb862b0ef7d800e8dfc
brockeetonCentralia <broc.keeton@student.centralia.edu>
2024-09-18 13:27:48 -0700

    Added Classes Component, Added route and navbar to classes.component.html

---
+++ src/app/classes/classes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-classes',
+  standalone: true,
+  imports: [NavBarComponent],
+  templateUrl: './classes.component.html',
+  styleUrls: ['./classes.component.css'],
+})
+export class ClassesComponent {
+
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { SurveyPageComponent } from "./survey-page/survey-page.component";
+import { ClassesComponent } from "./classes/classes.component";
 
 const routeConfig: Routes = [
     {
@@ -12,6 +13,11 @@ const routeConfig: Routes = [
         path: 'survey-page',
         component: SurveyPageComponent,
         title: 'Survey Page',
+    },
+    {
+        path: 'classes-page',
+        component: ClassesComponent,
+        title: 'Classes',
     },
 ];
 
