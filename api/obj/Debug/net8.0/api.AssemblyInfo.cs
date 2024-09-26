b8e992b00ab9ba0913fe54c0f9898c39988cd0de
brockeetonCentralia <broc.keeton@student.centralia.edu>
2024-09-26 15:58:28 -0700

    d

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e88decf88596d78bc6224db26dafe79e8d65347")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4989187f3ce64d5435a3abf7d4597500f77bd9fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
