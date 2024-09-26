4989187f3ce64d5435a3abf7d4597500f77bd9fb
brockeetonCentralia <broc.keeton@student.centralia.edu>
2024-09-26 15:51:05 -0700

    Merge frontendDev branch into mainDev

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40e0894c914241155e21b56c07a9ca13f2e49bc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e88decf88596d78bc6224db26dafe79e8d65347")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
